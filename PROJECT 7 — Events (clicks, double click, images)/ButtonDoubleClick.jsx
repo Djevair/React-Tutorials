5212efc5c654f1ba3290b633bbd24f372cc88d80
Djevair <139221010+Djevair@users.noreply.github.com>
2026-03-30 23:09:30 +0200

    Create ButtonDoubleClick.jsx

---
+++ PROJECT 7 — Events (clicks, double click, images)/ButtonDoubleClick.jsx
@@ -0,0 +1,7 @@
+function ButtonDoubleClick(){
+    const handleClick = (e) => e.target.textContent = "OUCH! 😢";
+    return(
+        <button onDoubleClick={(e) => handleClick(e)}>Click me 🤭</button>
+    )
+}
+export default ButtonDoubleClick;
