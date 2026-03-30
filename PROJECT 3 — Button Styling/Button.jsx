5be8f6e805915c6ebe7b95ba31525e0d91ada0eb
Djevair <139221010+Djevair@users.noreply.github.com>
2026-03-30 22:48:29 +0200

    Update Button.jsx

--- PROJECT 3 — Button Styling/Button.jsx
+++ PROJECT 3 — Button Styling/Button.jsx
@@ -6,12 +6,3 @@ function Button(){
 }
 export default Button;
 
-Modules
-import styles from './Button.module.css'
-function Button(){
-    return(
-        <button className={styles.button}>Click me</button>
-    );
-}
-
-export default Button;
