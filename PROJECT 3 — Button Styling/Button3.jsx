a89e7f03878fcd0e4307f4b600f0eb752ae97f81
Djevair <139221010+Djevair@users.noreply.github.com>
2026-03-30 22:49:39 +0200

    Create Button3.jsx

---
+++ PROJECT 3 — Button Styling/Button3.jsx
@@ -0,0 +1,16 @@
+//Inline
+function Button3(){
+    const styles = {
+        backgroundColor: "hsl(200, 100%, 50%)",
+        color: "white",
+        padding: "10px 20px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
+    }
+    return(
+        <button style={styles}>Click me</button>
+    );
+}
+
+export default Button3;
