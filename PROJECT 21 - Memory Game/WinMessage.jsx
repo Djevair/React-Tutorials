4b6fa151a280ef7ec0866e9cce5962fdcf981a1d
Djevair <139221010+Djevair@users.noreply.github.com>
2026-05-21 23:25:52 +0200

    Add files via upload

---
+++ PROJECT 21 - Memory Game/WinMessage.jsx
@@ -0,0 +1,10 @@
+const WinMessage = ({moves}) =>{
+    return (
+    <div className="win-message">
+        <h2>Congratulations</h2>
+        <p>You Completed the game in {moves} moves</p>
+    </div> 
+    )
+}
+
+export default WinMessage
