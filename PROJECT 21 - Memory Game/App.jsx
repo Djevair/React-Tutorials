a31b4d2583eecbb56dd14755e2dd0265dd8dbac2
Djevair <139221010+Djevair@users.noreply.github.com>
2026-05-21 23:23:45 +0200

    Add files via upload

---
+++ PROJECT 21 - Memory Game/App.jsx
@@ -0,0 +1,43 @@
+import { GameHeader } from "./components/GameHeader"
+import Card from "./components/Card";
+import WinMessage from "./components/WinMessage";
+import { useGameLogic } from "./hooks/useGameLogic";
+
+const cardValues = [
+  "💙",
+  "😢",
+  "💆",
+  "🤌",
+  "🐍",
+  "👟",
+  "🧑‍🦲",
+  "😭",
+  "💙",
+  "😢",
+  "💆",
+  "🤌",
+  "🐍",
+  "👟",
+  "🧑‍🦲",
+  "😭",
+];
+
+
+function App() {
+  const {cards,score,moves,isGameComplete, initializeGame, handleCardClick} = useGameLogic(cardValues)
+  return (
+    <>
+     <div className="app">
+        <GameHeader score={score} moves={moves} onReset={initializeGame}/>
+        {isGameComplete && <WinMessage moves={moves}/>}
+        <div className="cards-grid">
+          {cards.map((card) => (
+            <Card card={card} onClick={handleCardClick}/>
+          ))}
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default App
